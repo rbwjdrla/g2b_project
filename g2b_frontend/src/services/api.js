f24e41dade4efafa6b6d157374d4fe7219148bbb
rbwjdrla <maddux0782@gmail.com>
2025-12-05 07:05:25 +0000

    Update API URL

--- g2b_frontend/src/services/api.js
+++ g2b_frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://43.201.32.63:8000/api';
+const API_BASE_URL = 'http://54.180.93.48:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
